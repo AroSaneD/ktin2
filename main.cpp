16ec802df6b0941f0fda7760d3ea303a9577c4b1
AroSaneD <mail.junk.3135@gmail.com>
2015-04-14 15:29:55 +0300

    added sources to main file

--- main.cpp
+++ main.cpp
@@ -13,7 +13,13 @@
 using namespace std;
 
 
-
+/*
+to read up on:
+    http://stackoverflow.com/questions/13762673/how-to-program-an-ftp-client-in-c
+    http://tools.ietf.org/html/rfc959
+server:
+    hostinger.lt
+*/
 int main()
 {
     int serverSocket;
